52597224189c4f09b816f8e6583c6c03affc737c
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-03-06 13:24:29 +0100

    dfj

--- client/web/controllers/index-ctrl.js
+++ client/web/controllers/index-ctrl.js
@@ -6,102 +6,77 @@ App.controller('nuevaAppCtrl', ['$scope', function($scope) {
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctionsThe Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.benzinga.com/analyst-ratings/analyst-color/16/03/6797484/bofa-downgrades-alcoa-shares-to-neutral-following-recent"
 		},		
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.bidnessetc.com/64807-bank-of-america-merrill-lynch-cuts-rating-on-alcoa-inc/"
 		},
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.risersandfallers.com/2016/03/04/alcoa-inc-nyseaa-price-target-changes/"
 		},		
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.financial-market-news.com/alcoa-inc-aa-lowered-to-neutral-at-bank-of-america/919618/"
 		},
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.hilltopmhc.com/alcoa-inc-aa-stake-boosted-by-oppenheimer-co-inc/"
 		},		
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.thestreet.com/story/13481618/1/alcoa-aa-highlighted-as-todays-perilous-reversal-stock.html"
 		},
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.streetreport.co/technical-analysis-on-alcoa-inc-nyseaa/38931/"
 		},		
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.hilltopmhc.com/alcoa-inc-aa-debt-trading-4-5-higher-analyst/"
 		},
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.hilltopmhc.com/alcoa-inc-aa-shares-bought-by-oregon-public-employees-retirement-fund/"
 		},		
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.hilltopmhc.com/cowen-and-company-trims-alcoa-inc-aa-target-price-to-10-00/"
 		},
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.financial-market-news.com/q2-2016-eps-estimates-for-nucor-co-cut-by-keybanc-nue/924032/"
 		},		
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://news4j.com/top-stocks-of-the-day-nucor-corporation-nysenue-20/"
 		},
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.platts.com/latest-news/metals/pittsburgh/nucors-announced-steel-sheet-price-increase-gets-21038727"
 		},		
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.benzinga.com/analyst-ratings/analyst-color/16/02/6748101/keybancs-gibbs-cuts-rating-on-nucor-corp-after-new-guida"
 		},
 		{
 			title : "North Korea sanctions: Philippines to seize cargo ship1",
 			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
-		},		
-		{
-			title : "North Korea sanctions: Philippines to seize cargo ship1",
-			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
-		},
-		{
-			title : "North Korea sanctions: Philippines to seize cargo ship1",
-			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
-		},		
-		{
-			title : "North Korea sanctions: Philippines to seize cargo ship1",
-			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
-		},
-		{
-			title : "North Korea sanctions: Philippines to seize cargo ship1",
-			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
-		},		
-		{
-			title : "North Korea sanctions: Philippines to seize cargo ship1",
-			text : "The Philippines says it has seized a North Korean ship in line with tightened UN sanctions targeting the country's nuclear programme.",
-			link : "www.apio.com"
+			link : "http://www.financial-market-news.com/keybanc-analysts-decrease-earnings-estimates-for-nucor-co-nue/904138/"
 		}
 	];
 
